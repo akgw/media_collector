c3a69cc5623324d17ad22ff86c815891a4b20916
ryunosuke akagawa <ryu.akagawa@gmail.com>
2017-12-23 17:21:24 +0900

    add tweetserach

--- app/Http/Controllers/V1/TwitterController.php
+++ app/Http/Controllers/V1/TwitterController.php
@@ -3,13 +3,41 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use config\Twitter;
 use Illuminate\Http\Request;
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 
 class TwitterController extends Controller
 {
-    //
+    private $connection;
+
+    public function __construct()
+    {
+        $twitter = new Twitter();
+        $this->connection = new TwitterOauth($twitter->getConsumerKey(), $twitter->getConsumerSecret(), $twitter->getTwetterapi1(), $twitter->getTwetterapi2());
+    }
+
     public function index()
     {
-        return view('test');
+        $tweets_params = ['q' => '夜景,きれい OR キレイ OR 綺麗' ,'count' => '10'];
+
+        $response = $this->connection->get('search/tweets', $tweets_params);
+
+        if (empty($response->statuses)) {
+            return view('tweet');
+        }
+        $tweets = [];
+        foreach ($response->statuses as $key =>  $status) {
+            $tweets[$key] = [
+              'text' => $status->text,
+            ];
+        }
+
+
+
+        return view('tweet')->with('tweets', $tweets);
     }
+
+
 }
---
+++ config/twitter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace config;
+
+class Twitter
+{
+    private $CONSUMERSECRET;
+    private $CONSUMERKEY;
+    private $TWETTERAPI1;
+    private $TWETTERAPI2;
+
+
+    public function __construct() {
+        $this->CONSUMERKEY = env('CONSUMERKEY', '');
+        $this->CONSUMERSECRET = env('CONSUMERSECRET', '');
+        $this->TWETTERAPI1 = env('TWETTERAPI1', '');
+        $this->TWETTERAPI2 = env('TWETTERAPI2', '');
+    }
+
+    public function getConsumerKey() {
+        return $this->CONSUMERKEY;
+    }
+
+    public function getConsumerSecret() {
+        return $this->CONSUMERSECRET;
+    }
+
+    public function getTwetterapi1() {
+        return $this->TWETTERAPI1;
+    }
+
+    public function getTwetterapi2() {
+        return $this->TWETTERAPI2;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+    
+}
